b570e29ba9596933e42b910b2fa1c171ac7f6eab
César Gutiérrez Yáñez <cesargy@gmail.com>
2019-03-17 20:21:39 -0600

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cesar demo!';
+  mynumber = [{name: "Cesar"}, {name: "Karla"},{name: "Jim"}];
+  myData = fetch('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=00034b1ea16fb9bd9cb6a78d6e2c1176&format=json&limit=5')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
 }
